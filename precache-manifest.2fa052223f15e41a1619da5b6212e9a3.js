53b4dd49888bfc28236e7afd583d234ab26109f0
Jerry Wang <jerryw4113@gmail.com>
2020-04-12 01:22:05 +1000

    Updates

---
+++ precache-manifest.2fa052223f15e41a1619da5b6212e9a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d62c82e183df85d5ba70999451dea8",
+    "url": "/fake-screenshot-generator/index.html"
+  },
+  {
+    "revision": "befd9d9b85805458c96a",
+    "url": "/fake-screenshot-generator/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "befd9d9b85805458c96a",
+    "url": "/fake-screenshot-generator/static/js/2.3ac2d825.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/fake-screenshot-generator/static/js/2.3ac2d825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "274d24685a7bd0210533",
+    "url": "/fake-screenshot-generator/static/js/main.a94b1ffd.chunk.js"
+  },
+  {
+    "revision": "8c15d76308437a74f514",
+    "url": "/fake-screenshot-generator/static/js/runtime-main.1163d739.js"
+  }
+]);
